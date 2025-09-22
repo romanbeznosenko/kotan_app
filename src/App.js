b42119d44ba5ba6b868932c95ac64ade54a6662f
romanbeznosenko <245125@edu.p.lodz.pl>
2025-09-22 13:36:29 +0200

    Restructurized component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import TopNavigation from './components/common/TopNavigation';
+import TopNavigation from './components/common/TopNavigation/TopNavigation';
 
 function App() {
   return (
